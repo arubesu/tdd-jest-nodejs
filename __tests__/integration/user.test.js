01d96fc0f2b554db5f6f47a4e35b2b97c320cdc8
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-17 20:02:13 -0300

    TDD: RED - Add test to check password encrypt

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,20 +1,34 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 import truncate from '../util/truncate';
+import User from '../../src/app/models/User';
+
+function userMock() {
+  return {
+    name: 'Milford Rempel',
+    email: 'milfor1d@sample.com',
+    password_hash: '123456786675',
+  };
+}
 
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
   });
 
+  it('Should encrypt user password when new user is created', async () => {
+    const mock = userMock();
+    const user = await User.create(mock);
+
+    const hashResult = bcrypt.compare(mock.password, user.password_hash);
+    expect(hashResult).toBeTruthy();
+  });
+
   it('Should be able to create an user', async () => {
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Milford Rempel',
-        email: 'milfor1d@sample.com',
-        password_hash: '123456786675',
-      });
+      .send(userMock());
 
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('id');
@@ -23,19 +37,11 @@ describe('User', () => {
   it('Should not be able to create an user multiple times with the same email', async () => {
     await request(app)
       .post('/users')
-      .send({
-        name: 'Milford Rempel',
-        email: 'milford1@sample.com',
-        password_hash: '123456786675',
-      });
+      .send(userMock());
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Milford Rempel',
-        email: 'milford1@sample.com',
-        password_hash: '123456786675',
-      });
+      .send(userMock());
 
     expect(response.status).toBe(400);
   });

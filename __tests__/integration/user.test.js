ad9a38ce11f865af545f9dcfaa6ecf2d1de63072
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-17 19:44:51 -0300

    truncate database before tests

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,13 +1,18 @@
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('Should be able to create an user', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Milford Rempel',
-        email: 'milford@sample.com',
+        email: 'milfor1d@sample.com',
         password_hash: '123456786675',
       });
 

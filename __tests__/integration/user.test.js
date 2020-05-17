4e6bc4f6f083b887145a48a29a6f20c8c484e548
Bruno Souza <bruno.arubesu@gmail.com>
2020-05-17 19:18:53 -0300

    Add user test to check if user email is already registered

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -11,6 +11,27 @@ describe('User', () => {
         password_hash: '123456786675',
       });
 
+    expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should not be able to create an user multiple times with the same email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Milford Rempel',
+        email: 'milford1@sample.com',
+        password_hash: '123456786675',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Milford Rempel',
+        email: 'milford1@sample.com',
+        password_hash: '123456786675',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,13 +4,25 @@ class UserController {
   async store(req, res) {
     const { name, email, password_hash } = req.body;
 
+    const exists = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (exists) {
+      return res
+        .status(400)
+        .json({ error: 'This email is already registered' });
+    }
+
     const user = await User.create({
       name,
       email,
       password_hash,
     });
 
-    return res.json(user);
+    return res.status(201).json(user);
   }
 }
 
